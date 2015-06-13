97499e12c6697c97ce9a942484973903368a7f71
ired93 <anatolij-shergin@yandex.ru>
2015-06-13 20:23:18 +1000

    12.06.15
    sd

--- Client/Program.cs
+++ Client/Program.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
-
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading;
 
 namespace client
 {
@@ -20,7 +22,7 @@ namespace client
         static void Main(string[] args)
         {
             UdpClient receivingUdpClient = new UdpClient(clientUdpPort);
-            
+  
             IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, serverUdpPort);
             string stringData;
             do
@@ -37,7 +39,56 @@ namespace client
             TcpClient client = new TcpClient();
             client.Connect(server);
             client.Close();
+            ReceiveFile();
             
+        }
+        public static void Accept(IPEndPoint rmt)
+        {
+            IPEndPoint server = new IPEndPoint(rmt.Address, serverTcpPort);
+            TcpClient client = new TcpClient();
+            string apt = "Ok";
+            client.Connect(server);
+            byte[] byteString = Encoding.ASCII.GetBytes(apt);
+            
+            client.Client.SendTo(byteString, server);
+            client.Close();
+        }
+        public static void ReceiveFile()
+        {
+            int countSize = 8192;
+            UdpClient receivinfo = new UdpClient(clientUdpPort);
+            MD5CryptoServiceProvider csp = new MD5CryptoServiceProvider();
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, serverUdpPort);
+            byte[] byteCount = receivinfo.Receive(ref RemoteIpEndPoint);
+            string str = Encoding.ASCII.GetString(byteCount);
+            int M = Convert.ToInt32(str);      
+            receivinfo.Close();
+            Thread.Sleep(1000);
+            FileStream fs;
+            string hash = string.Empty;
+            UdpClient receive = new UdpClient(clientUdpPort);   
+            fs = new FileStream("temp.jpg", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+            fs.Position = 0;
+            for (int i = 1; i <= M; i++)
+            {
+                byte[] receiveBytes = receive.Receive(ref RemoteIpEndPoint);
+                fs.Write(receiveBytes, 0, receiveBytes.Length);
+                
+                byte[] byteHash = csp.ComputeHash(receiveBytes);
+                foreach (byte b in byteHash)
+                    hash += string.Format("{0:x2}", b);
+                fs.Position = i * countSize;
+            }
+            Console.WriteLine(hash);
+            Console.WriteLine("Данные получены ");
+            byte[] receiveHash = receive.Receive(ref RemoteIpEndPoint);
+            string receivedHash = Encoding.ASCII.GetString(receiveHash);
+            Console.WriteLine(receivedHash);
+            IPEndPoint rmt = RemoteIpEndPoint;
+            if (receivedHash == hash)
+                Accept(rmt);
+            receive.Close();
+            fs.Close();
         }
     }
 }
--- udpsrf2/Program.cs
+++ udpsrf2/Program.cs
@@ -3,20 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Net.Sockets;
 using System.Net;
 using System.Timers;
 using System.Threading;
+using System.IO;
+using System.Xml.Serialization;
+using System.Security.Cryptography;
 
 namespace udpsrf2
 {
     class Program
     {
+        [Serializable]
+        public class FileDetails
+        {
+            public string FILETYPE = "";
+            public long FILESIZE = 0;
+        }
+        private static FileDetails fileDet = new FileDetails();
         // Поля, связанные с UdpClient
         private static IPAddress remoteIPAddress;
         private const int remotePort = 5003;
         private static IPEndPoint endPoint;
-        
+
         public static UdpClient udp = new UdpClient();
         private static string HANDSHAKE = "Hello";
         private static Int32 serverUdpPort = 5001;
@@ -25,28 +36,28 @@ namespace udpsrf2
         private static Int32 clientTcpPort = 5004;
         private static TcpListener server = null;
         private static List<TcpClient> clients;
+        private static FileStream fs;
 
-        public static void AddClients()
+        public static void ClientAccepter()
         {
+            // принимает подключения, добавляя клиентов в глобальный список
             while (true)
             {
                 clients.Add(server.AcceptTcpClient());
-                Console.WriteLine("Connected " + clients.Last().Client.RemoteEndPoint.ToString());
+                Console.WriteLine("Client connected from " + clients.Last().Client.RemoteEndPoint.ToString());
             }
         }
         
 
         static void GetClients()
         {
-            
-
-            
-            // Ждем подключений клиентов по TCP
-            clients = new List<TcpClient>();
+            // готовим TCP-сервер
             server = new TcpListener(IPAddress.Any, serverTcpPort);
             server.Start();
-            Thread accepter = new Thread(new ThreadStart(AddClients));
-            accepter.Start();
+            clients = new List<TcpClient>();
+            Thread accepter = new Thread(new ThreadStart(ClientAccepter));
+            accepter.Start();  
+            
 
             // Начинаем перекличку по UDP
             remoteIPAddress = IPAddress.Broadcast;
@@ -56,18 +67,62 @@ namespace udpsrf2
             udp.Close();
             Console.WriteLine("Жду клиентов");
             Thread.Sleep(2000);
-
-
+            accepter.Interrupt();
+            
+            
         }
-
-        static void SendFile(string filename)
+        
+        private static void SendFile()
         {
+            UdpClient sender = new UdpClient();
+            UdpClient sendinfo = new UdpClient();
+            MD5CryptoServiceProvider csp = new MD5CryptoServiceProvider();
+            endPoint = new IPEndPoint(IPAddress.Broadcast, clientUdpPort);
+            
+            Console.WriteLine("Введите путь к файлу и его имя");
+            fs = new FileStream(@Console.ReadLine().ToString(), FileMode.Open, FileAccess.Read);
+            int countSize = 8192;
+            int fileCount = (int)fs.Length / countSize;
+            int M = fileCount / 2;
+            Byte[] bytes = new Byte[countSize];
+            string hash = string.Empty;
+
+            Console.WriteLine("Отправка " + M + " кусков");
+            Byte[] sendBytes = Encoding.ASCII.GetBytes(Convert.ToString(M));
+            sendinfo.Send(sendBytes, sendBytes.Length, endPoint);
+            sendinfo.Close();
+
+            Thread.Sleep(1000);
+
+            fs.Position = 0;
+            for (int i = 1; i <= M; i++)
+            {
+                fs.Read(bytes, 0, bytes.Length);
+                sender.SendAsync(bytes, bytes.Length, endPoint);
+                byte[] byteHash = csp.ComputeHash(bytes);
+                foreach (byte b in byteHash)
+                {
+                    hash += string.Format("{0:x2}", b);
+                }
+                
+                fs.Position = i * countSize;
+            }
+            Console.WriteLine(hash);
+            Byte[] hashBytes = Encoding.ASCII.GetBytes(hash);
+            sender.Send(hashBytes, hashBytes.Length, endPoint);
+            sender.Close();
+            fs.Close();
+            
           //  announce(filename);
         }
+        
 
         static void Main(string[] args)
         {
             GetClients();
+
+            SendFile();
+
         }
     }
 }
